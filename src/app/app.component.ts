8b010a3b6efbf54d6d51ea5f3553c81d4b8038f8
Adam Ashby <adzoashby@gmail.com>
2018-10-16 14:30:59 -0600

    app.component- moved router events subscription to constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Router, ActivationEnd } from '@angular/router';
+import { Router, ActivationEnd, ActivatedRouteSnapshot } from '@angular/router';
 import { ProjectData } from './interfaces/project-data';
 
 @Component({
@@ -8,31 +8,31 @@ import { ProjectData } from './interfaces/project-data';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   errorMessage: string;
   title: string;
   projectTitles: {[key: string]: string};
 
   constructor (
     private titleService: Title,
-    private router: Router) { }
-
-  ngOnInit() {
-    this.router.events.subscribe( e => {
+    private router: Router) {
+      this.router.events.subscribe( e => {
       if (e instanceof ActivationEnd) {
-        /* console.log(e.snapshot); */
-        this.title = e.snapshot.data.title;
-        if (e.snapshot.url[0].path === 'portfolio') {
-          if (!this.projectTitles) {
-            this.projectTitles = {};
-            e.snapshot.data.projectsData.projects.forEach( (proj: ProjectData) => {
-              this.projectTitles[proj.id] = proj.name;
-            });
-          }
-          this.title += ` [ ${this.projectTitles[e.snapshot.params.id]} ]`;
-        }
-        this.titleService.setTitle(this.title);
+        this.setTitle(e.snapshot);
       }
-    });
+    }
+
+  private setTitle(snapshot: ActivatedRouteSnapshot) {
+    this.title = snapshot.data.title;
+    if (snapshot.url[0].path === 'portfolio') {
+      if (!this.projectTitles) {
+        this.projectTitles = {};
+        snapshot.data.projectsData.projects.forEach( (proj: ProjectData) => {
+          this.projectTitles[proj.id] = proj.name;
+        });
+      }
+      this.title += ` [ ${this.projectTitles[snapshot.params.id]} ]`;
+    }
+    this.titleService.setTitle(this.title);
   }
 }
